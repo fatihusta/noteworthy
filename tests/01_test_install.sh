0f969f67c884c7fd58fcc052f5bb12cceb7b72c3
Mo Balaa <buddybalaa@gmail.com>
2020-03-16 18:21:21 -0500

    No tty for notectl install test

--- tests/01_test_install.sh
+++ tests/01_test_install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -it --entrypoint /opt/noteworthy/notectl/test-entrypoint.sh -v `pwd`:/opt/noteworthy/notectl python:3.8
+docker run --rm --entrypoint /opt/noteworthy/notectl/test-entrypoint.sh -v `pwd`:/opt/noteworthy/notectl python:3.8
