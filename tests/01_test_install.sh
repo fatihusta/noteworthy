eea46e18f7a0b4e348f10aeb74d9133bb5b2a82b
Mo Balaa <buddybalaa@gmail.com>
2020-03-16 18:44:43 -0500

    Add debug msg

--- tests/01_test_install.sh
+++ tests/01_test_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ls
+echo "Running notectl installation test..."
 
 docker run --rm --entrypoint /opt/noteworthy/notectl/test-entrypoint.sh -v `pwd`/notectl:/opt/noteworthy/notectl python:3.8
