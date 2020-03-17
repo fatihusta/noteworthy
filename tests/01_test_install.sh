3d56c4d2e8f46933cfe184c7204d77a06c3f5ddd
Mo Balaa <buddybalaa@gmail.com>
2020-03-16 19:14:47 -0500

    Add sleep

--- tests/01_test_install.sh
+++ tests/01_test_install.sh
@@ -2,4 +2,6 @@
 
 echo "Running notectl installation test..."
 
+sleep 10000
+
 docker run --rm --entrypoint /opt/noteworthy/notectl/test-entrypoint.sh -v `pwd`/notectl:/opt/noteworthy/notectl python:3.8
