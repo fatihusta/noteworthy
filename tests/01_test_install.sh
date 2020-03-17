5dbc480ca8f6b9a4d2b9aa974c6440b86e142aec
Mo Balaa <buddybalaa@gmail.com>
2020-03-16 20:51:39 -0500

    Fix tests locally

--- tests/01_test_install.sh
+++ tests/01_test_install.sh
@@ -2,5 +2,10 @@
 
 echo "Running notectl installation test..."
 
-
-docker run --volumes-from jenkins -e WORKSPACE=$WORKSPACE --rm --entrypoint $WORKSPACE/notectl/test-entrypoint.sh python:3.8
+if [ -z "$WORKSPACE" ]
+then
+    WORKSPACE=/opt/noteworthy
+    docker run -v `pwd`:/opt/noteworthy -e WORKSPACE=$WORKSPACE --rm --entrypoint $WORKSPACE/notectl/test-entrypoint.sh python:3.8
+else
+    docker run --volumes-from jenkins -e WORKSPACE=$WORKSPACE --rm --entrypoint $WORKSPACE/notectl/test-entrypoint.sh python:3.8
+fi
