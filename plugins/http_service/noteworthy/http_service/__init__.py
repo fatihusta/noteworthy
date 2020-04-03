057382f8fce3dc0b592cfadfa0f9ad574debae9e
Mo Balaa <buddybalaa@gmail.com>
2020-04-03 16:27:49 -0500

    Start django and grpc

--- plugins/http_service/noteworthy/http_service/__init__.py
+++ plugins/http_service/noteworthy/http_service/__init__.py
@@ -1,12 +1,27 @@
+import argparse
+import os
 from noteworthy.notectl.plugins import NoteworthyPlugin
 
 
 class HttpServiceController(NoteworthyPlugin):
 
-    PLUGIN_NAME = 'noteworthy-http-service'
+    PLUGIN_NAME = 'http_service'
 
     def __init__(self):
-        pass
+        super().__init__(__file__)
 
+    def run(self, *args, **kwargs):
+        os.chdir(os.path.join(self.plugin_path, 'rest_api'))
+        os.system('python manage.py runserver 0.0.0.0:8001')
+
+    def start(self, **kwargs):
+        self._start(self.PLUGIN_NAME)
+
+    @classmethod
+    def _setup_argparse(cls, arg_parser):
+        super()._setup_argparse(arg_parser)
+        cls.sub_parser = argparse.ArgumentParser(conflict_handler='resolve',
+        usage='notectl http_service')
+        cls.sub_parser.add_argument('argument', nargs='*', help='hostname of hub to join')
 
 Controller = HttpServiceController
