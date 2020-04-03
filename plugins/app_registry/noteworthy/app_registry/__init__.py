83509674c55253dab264d00c9fb0244d4fa1df47
Patrick De La Garza <Patrickdlg@gmail.com>
2020-04-02 17:18:30 -0700

    save app-registry skeleton

--- plugins/app_registry/noteworthy/app_registry/__init__.py
+++ plugins/app_registry/noteworthy/app_registry/__init__.py
@@ -1,11 +1,33 @@
+import argparse
+import docker
+import os
+import sys
+
+import getpass
+
 from noteworthy.notectl.plugins import NoteworthyPlugin
+from grpcz import grpc_controller, grpc_method
 
 
+@grpc_controller
 class AppRegistryController(NoteworthyPlugin):
 
     PLUGIN_NAME = 'noteworthy-app-registry'
 
     def __init__(self):
+        pass
+
+    def build(self, **kwargs):
+        # TODO: build the container
+        pass
+
+    def start():
+        # TODO: start the server
+        pass
+
+    @grpc_method
+    def update_app(self, **kwargs):
+        # TODO: write file and update yml
         pass
 
 
