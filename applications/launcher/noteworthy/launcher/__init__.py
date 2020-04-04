a92194f283ae6c96ea35f0ddd66f2aa864bd4257
Patrick De La Garza <Patrickdlg@gmail.com>
2020-04-04 14:29:09 -0700

    now injecting args

--- applications/launcher/noteworthy/launcher/__init__.py
+++ applications/launcher/noteworthy/launcher/__init__.py
@@ -19,24 +19,28 @@ class LauncherController(NoteworthyPlugin):
         self.docker = docker.from_env()
 
     def install(self, **kwargs):
-        self.sub_parser.add_argument('--archive', help='path of archive to install')
+        self.sub_parser.add_argument(
+            '--archive', help='path of archive to install')
         args = self.sub_parser.parse_known_args(self.args)[0]
         if args.archive:
-            self.app, self.version = os.path.basename(args.archive).split('-')
-            self.version = self.version.replace('.tar.gz', '')
-            self.app_dir = os.path.join(self.PACKAGE_CACHE, f'{self.app}/{self.version}')
-            self.deploy_dir = os.path.join(self.plugin_path, 'deploy')
+            app, version = os.path.basename(args.archive).split('-')
+            version = version.replace('.tar.gz', '')
+            app_dir = os.path.join(self.PACKAGE_CACHE, f'{app}/{version}')
+            deploy_dir = os.path.join(self.plugin_path, 'deploy')
             Path(self.PACKAGE_CACHE).mkdir(parents=True, exist_ok=True)
             shutil.unpack_archive(args.archive, self.PACKAGE_CACHE)
             for file in ['Dockerfile', 'install.sh', 'app.yaml']:
-                shutil.copyfile(os.path.join(self.deploy_dir, file), os.path.join(self.app_dir, file))
-            self.build_container()
+                shutil.copyfile(os.path.join(deploy_dir, file),
+                                os.path.join(app_dir, file))
+            self._build_container(app_dir, app, version)
         else:
-            raise NotImplementedError('Installing from repository not supported yet.')
+            raise NotImplementedError(
+                'Installing from repository not supported yet.')
         print('Done.')
 
-    def build_container(self, **kwargs):
-        self.docker.images.build(path=self.app_dir, tag=f'noteworthy-{self.app}:{self.version}')
+    def _build_container(self, app_dir, app, version):
+        self.docker.images.build(
+            path=app_dir, tag=f'noteworthy-{app}:{version}')
 
 
 Controller = LauncherController
