45afce6b7569e0d21e275f5d787eba21bddcd504
Mo Balaa <buddybalaa@gmail.com>
2020-04-05 18:45:22 -0500

    Fix duplicate package declaration

--- applications/messenger/setup.py
+++ applications/messenger/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_namespace_packages
 
-setup(name='noteworthy-launcher',
+setup(name='noteworthy-messenger',
       url="https://noteworthy.im",
       author_email="hi@decentralabs.io",
       version='0.0.1',
       packages=find_namespace_packages(include=['noteworthy.*']),
-      entry_points={'notectl.plugins':  'launcher = noteworthy.launcher'},
+      entry_points={'notectl.plugins':  'messenger = noteworthy.messenger'},
       # namespace packages wont work without zip_safe=False
       zip_safe=False,
       install_requires=[]
