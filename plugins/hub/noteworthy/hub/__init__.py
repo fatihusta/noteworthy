3ac82ce36c4d8c54f1ea818450149b28bc46c02b
Mo Balaa <buddybalaa@gmail.com>
2020-04-05 04:34:20 -0500

    Refactor grpcz to use explicit protobuf messages types for request, response. Add request response to decorator.

--- plugins/hub/noteworthy/hub/__init__.py
+++ plugins/hub/noteworthy/hub/__init__.py
@@ -1,6 +1,10 @@
 from noteworthy.notectl.plugins import NoteworthyPlugin
 
+from grpcz import grpc_controller, grpc_method
 
+from noteworthy.hub.proto.messages_pb2 import ReservationRequest, ReservationResponse
+
+@grpc_controller
 class HubController(NoteworthyPlugin):
 
     PLUGIN_NAME = 'noteworthy-hub'
@@ -8,6 +12,10 @@ class HubController(NoteworthyPlugin):
 
     def __init__(self):
         pass
+
+    @grpc_method(ReservationRequest, ReservationResponse)
+    def reserve_domain(self, domain: str, pub_key: str, auth_code: str):
+        return {"endpoint": '10.0.0.1', "port":1557, "ip_assignment":"10.0.0.2"}
 
 
 Controller = HubController
