bd1170b5a1f3edad30318b5dcf7ed5965131dac4
Mo Balaa <buddybalaa@gmail.com>
2020-04-06 11:06:30 -0500

    working link provisioning

--- plugins/hub/noteworthy/hub/__init__.py
+++ plugins/hub/noteworthy/hub/__init__.py
@@ -1,5 +1,7 @@
 from noteworthy.notectl.plugins import NoteworthyPlugin
 
+import docker
+
 from grpcz import grpc_controller, grpc_method
 
 from noteworthy.hub.proto.messages_pb2 import ReservationRequest, ReservationResponse
@@ -11,10 +13,23 @@ class HubController(NoteworthyPlugin):
     DJANGO_APP_MODULE = 'noteworthy.hub.api'
 
     def __init__(self):
-        pass
+        self.docker = docker.from_env()
 
     @grpc_method(ReservationRequest, ReservationResponse)
     def reserve_domain(self, domain: str, pub_key: str, auth_code: str):
+        volumes = []
+        ports = {'18521/udp': None}
+        container_name = domain.replace('.', '-')
+        self.docker.containers.run(f'noteworthy-launcher:DEV',
+        tty=True,
+        cap_add=['NET_ADMIN'],
+        network='noteworthy',
+        stdin_open=True,
+        name=f"link-{container_name}",
+        #auto_remove=True,
+        volumes=volumes,
+        ports=ports,
+        detach=True)
         return {"endpoint": '10.0.0.1', "port":1557, "ip_assignment":"10.0.0.2"}
 
 
