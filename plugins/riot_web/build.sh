351257d645b8dc98cb6a5d5cbe38c5a5a6fe52c7
Patrick De La Garza <Patrickdlg@gmail.com>
2020-04-06 12:03:08 -0700

    now building and bundling riot-web noteworthy

--- plugins/riot_web/build.sh
+++ plugins/riot_web/build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+set -e
 echo 'Building riot_web plugin...'
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+docker build ext -t riot_web_tmp_img
+echo "build it"
+docker create -ti --name riot_web_tmp riot_web_tmp_img bash
+docker cp riot_web_tmp:/src/web_app.tar.gz ext/web_app.tar.gz
+docker rm -f riot_web_tmp
 echo 'done.'
