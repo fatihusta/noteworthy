3c1da1a072bc1c169197f7eeae028bb38fdded5c
Mo Balaa <buddybalaa@gmail.com>
2020-04-07 16:50:38 -0500

    Add _reload method to nginx controller; add serve_domain implementation prototype

--- plugins/nginx/noteworthy/nginx/__init__.py
+++ plugins/nginx/noteworthy/nginx/__init__.py
@@ -16,4 +16,14 @@ class NginxController(NoteworthyPlugin):
     def start(self, **kwargs):
         self._start(self.PLUGIN_NAME)
 
+
+    def _reload(self, **kwargs):
+        os.system('nginx -s reload')
+
+    def serve_domain(self, domain, config, get_certs=True):
+        if get_certs:
+            cert_info = self._cerbot(domain)
+        self._write_config(domain, config, cert_info)
+        self._reload()
+
 Controller = NginxController
