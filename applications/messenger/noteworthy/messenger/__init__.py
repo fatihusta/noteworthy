cf901c44d58a472432c03950dbf196189843771d
Patrick De La Garza <Patrickdlg@gmail.com>
2020-04-08 16:48:02 -0700

    now messenger app runs a synapse homeserver

--- applications/messenger/noteworthy/messenger/__init__.py
+++ applications/messenger/noteworthy/messenger/__init__.py
@@ -1,15 +1,62 @@
+import os
+import string
+import secrets
+from jinja2 import Template
+from synapse.app import homeserver
 from noteworthy.notectl.plugins import NoteworthyPlugin
+from noteworthy.notectl.plugins import PluginManager
 
 
 class MessengerController(NoteworthyPlugin):
 
     PLUGIN_NAME = 'messenger'
 
+    SECRET_ALPHABET = string.digits + string.ascii_letters + ".,;^&*-_+=#~@"
+    SECRET_LENGTH = 50
+
     def __init__(self):
         super().__init__(__file__)
+        self.plugins = PluginManager.load_plugins()
 
     def run(self, *args, **kwargs):
-        pass
+        configs = {'domain': os.environ['NOTEWORTHY_DOMAIN']}
+        if self.is_first_run:
+            self._run_first_time_setup()
+        hs_config = os.path.join(self.config_dir, 'homeserver.yaml')
+        os.system(f'synctl start {hs_config}')
+
+    def _run_first_time_setup(self):
+        self.create_config_dir()
+        riot = self.plugins['riot_web'].Controller()
+        web_client_location = os.path.join(riot.config_dir, 'webapp')
+        configs = {
+            'domain': os.environ['NOTEWORTHY_DOMAIN'],
+            'registration_shared_secret': self._get_new_secret(),
+            'macaroon_secret_key': self._get_new_secret(),
+            'form_secret': self._get_new_secret(),
+            'web_client_location': web_client_location,
+            'config_dir': self.config_dir}
+        homeserver_tmpl = os.path.join(
+            self.deploy_dir, 'homeserver.tmpl.yaml')
+        homeserver_target = os.path.join(
+            self.config_dir, 'homeserver.yaml')
+        self._generate_file_from_template(
+            homeserver_tmpl, homeserver_target, configs)
+        homeserver.setup(
+            ['-c', homeserver_target, '--generate-missing-configs'])
+
+    def _generate_file_from_template(self, tmpl_path, target, configs):
+        with open(tmpl_path, 'r') as f:
+            tmpl = Template(f.read())
+        rendered = tmpl.render(configs)
+        with open(target, 'w') as f:
+            f.write(rendered)
+
+    def _get_new_secret(self):
+        return ''.join((
+            secrets.choice(self.SECRET_ALPHABET)
+            for i
+            in range(self.SECRET_LENGTH)))
 
     def start(self, *args, **kwargs):
         self._start(self.PLUGIN_NAME)
--- plugins/riot_web/noteworthy/riot_web/__init__.py
+++ plugins/riot_web/noteworthy/riot_web/__init__.py
@@ -1,17 +1,21 @@
 import tarfile
+import os
 from noteworthy.notectl.plugins import NoteworthyPlugin
 
 
 class RiotWebController(NoteworthyPlugin):
 
-    PLUGIN_NAME = 'noteworthy-riot-web'
+    PLUGIN_NAME = 'riot_web'
 
     def __init__(self):
         super().__init__(__file__)
 
     def start(self, *args, **kwargs):
-        with tarfile.open(f'{self.plugin_path}/deploy/web_app.tar.gz') as tar:
-            tar.extractall(path='/riot-web')
+        if self.is_first_run:
+            tar_path = os.path.join(self.deploy_dir, 'web_app.tar.gz')
+            with tarfile.open(tar_path) as tar:
+                self.create_config_dir()
+                tar.extractall(path=self.config_dir)
 
 
 Controller = RiotWebController
