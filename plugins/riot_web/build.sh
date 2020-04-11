8c392854a0b90659e13e9a2de19f896915af2706
Patrick De La Garza <Patrickdlg@gmail.com>
2020-04-11 12:08:08 -0700

    Download Pre-Built Riot Web

---
+++ plugins/riot_web/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+set -e
+echo 'Downloading riot_web app...'
+curl https://decentralabs.io/download/riot_web_app.tar.gz > noteworthy/riot_web/deploy/web_app.tar.gz
+echo 'Finished Downloading riot_web: noteworthy/riot_web/deploy/web_app.tar.gz'
