8a43c376afa6eb899c04c5330d92ff861571b632
Mo Balaa <buddybalaa@gmail.com>
2020-04-11 11:23:55 -0500

    Only install package and launcher on prod release container

--- docker-prod.sh
+++ docker-prod.sh
@@ -9,12 +9,16 @@ rm -rf build/ dist/
 python setup.py install
 
 # TODO don't install every plugin
-for plugin in plugins/*/; do
-    cd $plugin
-    rm -rf build/ dist/
-    python setup.py install
-    cd -
-done
+# for plugin in plugins/*/; do
+#     cd $plugin
+#     rm -rf build/ dist/
+#     python setup.py install
+#     cd -
+# done
+
+cd /opt/noteworthy/notectl/plugins/package
+rm -rf build/ dist/
+python setup.py install
 
 cd /opt/noteworthy/notectl/applications/launcher
 rm -rf build/ dist/
