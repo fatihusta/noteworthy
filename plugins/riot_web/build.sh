e61085ea17b1eafc3c111a718314c40697b8dd48
Patrick De La Garza <Patrickdlg@gmail.com>
2020-04-11 12:24:46 -0700

    fix extraction path

--- plugins/riot_web/build.sh
+++ plugins/riot_web/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 echo 'Downloading riot_web app...'
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
 curl https://decentralabs.io/download/riot_web_app.tar.gz > noteworthy/riot_web/deploy/web_app.tar.gz
 echo 'Finished Downloading riot_web: noteworthy/riot_web/deploy/web_app.tar.gz'
