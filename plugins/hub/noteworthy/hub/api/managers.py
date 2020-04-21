c052c25cd19d7cbc610d2f37505884e8951a1b4c
Patrick De La Garza <Patrickdlg@gmail.com>
2020-04-21 16:59:47 -0700

    Add BetaUser + Manager

---
+++ plugins/hub/noteworthy/hub/api/managers.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import ugettext_lazy as _
+
+
+class BetaUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_unusable_password()
+        user.save()
+        return user
--- plugins/hub/noteworthy/hub/api/models.py
+++ plugins/hub/noteworthy/hub/api/models.py
@@ -1,9 +1,24 @@
 import uuid
-
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+
+from .managers import BetaUserManager
 
 
 class Reservation(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     domain = models.CharField(max_length=255)
 
+class BetaUser(AbstractBaseUser):
+    email = models.EmailField(_('email address'), unique=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = BetaUserManager()
+
+    def __str__(self):
+        return self.email
