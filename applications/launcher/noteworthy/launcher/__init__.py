749fea758a6dba767de8a74225718f32470d30de
Mo Balaa <buddybalaa@gmail.com>
2020-04-28 10:24:17 -0500

    unify dockerfiles

--- applications/launcher/noteworthy/launcher/__init__.py
+++ applications/launcher/noteworthy/launcher/__init__.py
@@ -125,7 +125,7 @@ class LauncherController(NoteworthyPlugin):
             Path(self.PACKAGE_CACHE).mkdir(parents=True, exist_ok=True)
             shutil.unpack_archive(archive_path, self.PACKAGE_CACHE)
             shutil.copyfile(os.path.join(deploy_dir, 'install.sh'), os.path.join(app_dir, 'install.sh'))
-            shutil.copyfile(os.path.join(deploy_dir, f'Dockerfile.{env}'), os.path.join(app_dir, 'Dockerfile'))
+            shutil.copyfile(os.path.join(deploy_dir, 'Dockerfile'), os.path.join(app_dir, 'Dockerfile'))
             self._build_container(app_dir, app, version)
 
             # deploy launcher / launcher-hub
