61536867e93e5c3934ed61bb5d8fe2a454bf38a8
Mo Balaa <buddybalaa@gmail.com>
2020-05-14 17:38:48 -0500

    fix iptables rules

--- link-entrypoint.sh
+++ link-entrypoint.sh
@@ -16,6 +16,8 @@ wg set wg0 listen-port 18521
 ip addr add 10.0.0.1/24 dev wg0
 ip link set wg0 up
 
+wg set wg0 peer $TAPROOT_PUBKEY allowed-ips 10.0.0.2/32
+
 #iptables forward port 80, 443 to 10.0.0.2:80/443
 iptables -A FORWARD -i eth0 -o wg0 -p tcp --syn --dport 80 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A FORWARD -i eth0 -o wg0 -p tcp --syn --dport 443 -m conntrack --ctstate NEW -j ACCEPT
@@ -23,13 +25,12 @@ iptables -A FORWARD -i eth0 -o wg0 -p tcp --syn --dport 443 -m conntrack --ctsta
 iptables -A FORWARD -i eth0 -o wg0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -i wg0 -o eth0 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
-iptables -P FORWARD DROP
 
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 80 -j DNAT --to-destination 10.0.0.2
 iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 443 -j DNAT --to-destination 10.0.0.2
 
-iptables -t nat -A POSTROUTING -o wg0 -p tcp --dport 80 -j SNAT --to-source 10.0.0.2
-iptables -t nat -A POSTROUTING -o wg0 -p tcp --dport 443 -j SNAT --to-source 10.0.0.2
+iptables -t nat -A POSTROUTING -o wg0 -p tcp --dport 80 -j SNAT --to-source 10.0.0.1
+iptables -t nat -A POSTROUTING -o wg0 -p tcp --dport 443 -j SNAT --to-source 10.0.0.1
 
 # vpn app udp proxies
 socat UDP4-RECVFROM:18522,fork UDP4-SENDTO:10.0.0.2:18522,sp=18524,reuseaddr &
